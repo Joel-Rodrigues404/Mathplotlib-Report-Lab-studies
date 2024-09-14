3e602693b5cfa33b52676ad563dd560aa6610b1f
Joel-Rodrigues404 <joelsandersonrodrigues@gmail.com>
2024-09-14 18:30:33 -0300

    pdfs

--- matplotlib_codes/bar_chart.py
+++ matplotlib_codes/bar_chart.py
@@ -8,10 +8,12 @@ dirpath = "charts"
 
 
 bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange', '#00fff0']
-bar_labels = ['red', 'blue', 'red', 'orange', 'blue', 'red', 'orange', 'blue', 'orange', 'blue']
+bar_labels = ['red', 'blue', 'red', 'orange', 'blue',
+              'red', 'orange', 'blue', 'orange', 'blue']
 
 
-x = ["algo", "algo2", "algo3", "algo4", "algo5", "algo6", "algo7", "algo8", "algo9", "algo10"]
+x = ["algo", "algo2", "algo3", "algo4", "algo5",
+     "algo6", "algo7", "algo8", "algo9", "algo10"]
 y = [randint(1, 1000) for x in range(10)]
 
 
@@ -19,7 +21,8 @@ plt.figure(figsize=[15, 10])
 plt.bar(x, y, edgecolor="white", color=bar_colors, label=bar_labels)
 plt.ylabel("Count", fontsize=20, fontweight='bold')
 # plt.xticks(rotation=23, fontweight='bold')
-plt.xticks(range(len(x)), rotation=23, fontsize=12, color='black', fontweight='bold', ha='center')
+plt.xticks(range(len(x)), rotation=23, fontsize=12,
+           color='black', fontweight='bold', ha='center')
 
 plt.legend(title='legenda', loc='upper right', bbox_to_anchor=(1.1, 1.013))
 
